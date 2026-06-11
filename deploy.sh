07d141ddcdac661e1475c0860eb1b92528930c8e
MF-swiss <marco.fritsche@outlook.com>
2026-06-11 17:01:14 +0200

    jira

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+set -e
+
+echo "Starting Jira..."
 docker compose -f jira/docker-compose.yml up -d
+
+echo "Starting Portainer..."
 docker compose -f portainer/docker-compose.yml up -d
+
+echo "All services started."
